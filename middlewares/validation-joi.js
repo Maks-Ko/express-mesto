a699021716d2724fc5d2f0adfc12f41ee7b6e772
Maksim Kozhevnikov <kozhevnikow@yandex.ru>
2021-12-05 09:20:07 +0500

    fefactor: joi avatar

--- middlewares/validation-joi.js
+++ middlewares/validation-joi.js
@@ -8,7 +8,7 @@ const validationCreateUser = celebrate({
     password: Joi.string().required().min(8),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    avatar: Joi.string().pattern(/^((http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-/])*)?/),
   }),
 });
 
